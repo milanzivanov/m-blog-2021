e1df68c681f407a2400991ba7137bebf6ed99a4a
milan.zivanov <milan.zivanov@gmail.com>
2021-02-18 11:40:20 +0100

    #5 - Multiple Components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
 
-  const title = "Welcome to the new blog";
-  const likes = 38;
-  // const person = {
-  //   name: 'milan',
-  //   age: 38
-  // }
-  const link = "http://google.com";
-
   return (
     <div className="App">
+      <Navbar></Navbar>
       <div className="content">
-        <h1>{ title }</h1>
-        <p>Liked {likes} times</p>
-        {/* <p>{person}</p> */}
-        <p>{19}</p>
-        <p>{"Hello Milan"}</p>
-        <p>{1,2,3,4,5}</p>
-        <p>{(Math.random() * 100).toFixed()}</p>
-        <a href={link}>Google site</a>
+        <Home></Home>
       </div>
     </div>
   );
