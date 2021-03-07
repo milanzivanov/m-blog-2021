d9c34ee26979ca0b76cdb20156f4f80770159389
milan.zivanov <milan.zivanov@gmail.com>
2021-03-07 17:06:47 +0100

    Full React Tutorial #18 - Conditional Loading Message

--- src/Home.js
+++ src/Home.js
@@ -5,17 +5,24 @@ import BlogList from './BlogList';
 function Home(props) {
 
     const [blogs, setBlogs] = useState(null);
+    const [isPanding, setIspanding] = useState(true);
 
     useEffect(() => {
 
         // my way with my json file
         const fetchData = async () => {
+            
             const response = await fetch("https://next.json-generator.com/api/json/get/4ydcDgPG9");
             const data = await response.json();
             let dataResponse = data.blogs;
-            console.log(dataResponse);
+            // console.log(dataResponse);
             setBlogs(dataResponse);
+
+            // to hide loading msg
+            setIspanding(false);
+
         };
+        
         fetchData();
 
         // second way 
@@ -30,6 +37,10 @@ function Home(props) {
 
     return (
         <div className="home">
+
+            {/* to show loading msg */}
+            { isPanding &&  <div>Loading...</div> }
+
             {blogs && <BlogList 
                 blogs={blogs} 
                 title="Marko's blog">
