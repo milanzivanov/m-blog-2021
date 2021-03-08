1e0f9a2520e9d5d3a04294ea1030244669150d30
milan.zivanov <milan.zivanov@gmail.com>
2021-03-08 11:01:11 +0100

    Full React Tutorial #19 - Handling Fetch Errors

--- src/Home.js
+++ src/Home.js
@@ -6,38 +6,71 @@ function Home(props) {
 
     const [blogs, setBlogs] = useState(null);
     const [isPanding, setIspanding] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
 
         // my way with my json file
-        const fetchData = async () => {
+        // const fetchData = async () => {
+
+        //     try {
+        //         const response = await fetch("https://next.json-generator.com/api/json/get/4ydcDgPG9");
+
+            // if(!res.ok) {
+            //     throw Error("Could not fetch the data for that resource");
+            // }
+
+                // const data = await response.json();
+                // let dataResponse = data.blogs;
+                // setBlogs(dataResponse);
+    
+                // // to hide loading msg
+                // setIspanding(false);
+
+                // // error
+                // setError(null);
+
+        //     } catch(err) {
+                    // setIspanding(false);
+                    // setError(err.message);
+        //          console.log(err); // TypeError: failed to fetch
+        //     }
             
-            const response = await fetch("https://next.json-generator.com/api/json/get/4ydcDgPG9");
-            const data = await response.json();
-            let dataResponse = data.blogs;
-            // console.log(dataResponse);
-            setBlogs(dataResponse);
-
-            // to hide loading msg
-            setIspanding(false);
-
-        };
+        // };
         
-        fetchData();
+        // fetchData();
 
-        // second way 
-        // fetch("http://localhost:8000/blogs")
-        //     .then( res => res.json())
-        //     .then(data => {
-        //         console.log(data);
-        //         setBlogs(data);
-        //     });
+        // second way tutorial way
+        // start json-server: npx json-server --watch data/db.json --port 8000
+        fetch("http://localhost:8000/blogs")
+            .then( res => {
+                if(!res.ok) {
+                    throw Error("Could not fetch the data for that resource");
+                }
+                return res.json()
+            })
+            .then(dataResponse => {
+                setBlogs(dataResponse);
+
+                // to hide loading msg
+                setIspanding(false);
+
+                // error
+                setError(null);
+            })
+            .catch(err => {
+                setIspanding(false);
+                // console.log(err.message)
+                setError(err.message);
+            });
 
     }, []);
 
     return (
         <div className="home">
 
+            {/* error state */}
+            {  error && <div>{ error }</div>}
             {/* to show loading msg */}
             { isPanding &&  <div>Loading...</div> }
 
