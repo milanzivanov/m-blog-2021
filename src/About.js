02b687256123e7fa14382e630e21e9ed0a7e61c8
milan.zivanov <milan.zivanov@gmail.com>
2021-03-22 11:31:04 +0100

    Full React Tutorial #27 - Controlled Inputs (forms)

--- src/About.js
+++ src/About.js
@@ -1,7 +1,47 @@
+import { useState } from "react";
+
 const About = () => {
+
+const [title, setTitle] = useState('');
+const [body, setBody] = useState('');
+const [author, setAuthor] = useState('marko');
+
+
     return ( 
         <div className="about">
-            <h2>My name is for now nn</h2>
+            <h2>Add new blog</h2>
+            <form>
+                <label>Blog title:</label>
+                <input 
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+
+                <label>Blog body:</label>
+                <textarea 
+                    required
+                    volue={body}
+                    onChange={(e) => setBody(e.target.value)}
+                ></textarea>
+
+                <label>Blog author:</label>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="marko">mario</option>
+                    <option value="misa">misa</option>
+                </select>
+
+                <button>
+                    Add Blog
+                </button>
+                <p>{ title }</p>
+                <p>{ body }</p>
+                <p>{ author }</p>
+            </form>
         </div>
     );
 }
